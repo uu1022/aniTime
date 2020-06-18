eb42e91855e28774a698a0bc516f7a17e0adff70
uu1022 <before_liyu@163.com>
2020-06-18 08:35:40 +0800

    Initial Commit

---
+++ aniTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aniTime
+//
+//  Created by 张聪 on 2020/6/18.
+//  Copyright © 2020 李煜. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
